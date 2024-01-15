feaba2e4fa58c14b0923821018869f075d56fbed
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-01-15 23:57:04 +0100

    Prepare K8s builder

--- src/run.ts
+++ src/run.ts
@@ -20,6 +20,7 @@ type Inputs = {
   push: boolean
   tags: string[]
   target: string
+  kubernetesNamespace: string
 }
 
 type Outputs = {
@@ -27,33 +28,116 @@ type Outputs = {
 }
 
 export async function run(inputs: Inputs): Promise<Outputs> {
+  const contextDir = resolve(inputs.context)
+  const dockerConfigDir = resolve('~/.docker')
   const runnerTempDir = process.env.RUNNER_TEMP || tmpdir()
-  const outputsDir = await mkdtemp(join(runnerTempDir, 'kaniko-action'))
+  const outputsDir = await mkdtemp(join(runnerTempDir, 'kaniko-action-outputs'))
 
-  const args = generateArgs(inputs, outputsDir)
+  // TODO: Build kaniko args
+  const args = generateArgs(inputs)
 
-  const start = Date.now()
-  await exec('go', args)
-  const end = Date.now()
-  const seconds = (end - start) / 1000
-  info(`Built in ${seconds}s`)
+  // TODO: Start executor pod
+  // kubectl create -f kaniko.k8s.yml
+  await exec('kubectl', ['apply', '-f', '-'], {
+    input: Buffer.from(
+      `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: test-kaniko
+  namespace: ajjxp
+spec:
+  initContainers:
+    - name: prepare
+      image: devodev/inotify:0.1.0
+      command:
+      - inotifywait
+      - -e
+      - create
+      - /ready
+      resources:
+        limits:
+          cpu: '0.2'
+          memory: '10Mi'
+      volumeMounts:
+        - name: ready
+          mountPath: /ready
+        - name: docker-config
+          mountPath: /docker-config
+        - name: context
+          mountPath: /context
+  containers:
+    - name: build
+      image: gcr.io/kaniko-project/executor:v1.19.2
+      args:
+        - --context=dir:///context
+        - --dockerfile=Dockerfile
+        - --no-push
+      resources:
+        limits:
+          cpu: '1'
+          memory: '10Gi'
+      volumeMounts:
+        - name: docker-config
+          mountPath: /kaniko/.docker
+        - name: context
+          mountPath: /context
+  volumes:
+  - name: ready
+    emptyDir:
+      sizeLimit: 1Ki
+  - name: docker-config
+    emptyDir:
+      sizeLimit: 1Gi
+  - name: context
+    emptyDir:
+      sizeLimit: 100Gi
+  restartPolicy: Never
+      `,
+      'utf8',
+    ),
+  })
+
+  // TODO: Wait for exec container
+  await delay(5000)
+
+  // TODO: Upload build context
+  // tar -czf - -C tests/fixtures/ . | kubectl exec -i test-kaniko -c prepare -- tar -xzf - -C /context
+  await exec('sh', [
+    '-c',
+    `tar -czf - -C ${contextDir} . | kubectl exec -i test-kaniko -c prepare -- tar -xzf - -C /context`,
+  ])
+
+  // TODO: Upload .docker folder
+  // tar -czf - -C ~/.docker/ . | kubectl exec -i test-kaniko -c prepare -- tar -xzf - -C /docker-config
+  await exec('sh', [
+    '-c',
+    `tar -czf - -C ${dockerConfigDir} . | kubectl exec -i test-kaniko -c prepare -- tar -xzf - -C /docker-config`,
+  ])
+
+  // TODO: Create ready file
+  // kubectl exec test-kaniko -c prepare -- touch /ready/yes
+  await exec('kubectl', ['exec', 'test-kaniko', '-c', 'prepare', '--', 'touch', '/ready/yes'])
+
+  // TODO: Wait for pod completion
+  // kubectl wait pod test-kaniko --for condition=Ready --timeout=5m
+
+  // TODO: Delete pod
+  // kubectl delete pod test-kaniko
+
+  // const start = Date.now()
+  // await exec('go', args)
+  // const end = Date.now()
+  // const seconds = (end - start) / 1000
+  // info(`Built in ${seconds}s`)
 
   const digest = (await readFile(`${outputsDir}/digest`)).toString().trim()
   info(digest)
   return { digest }
 }
 
-function generateArgs(inputs: Inputs, outputsDir: string): string[] {
-  const args = [
-    // go args
-    'run',
-    'github.com/GoogleContainerTools/kaniko/cmd/executor@v1.19.2',
-    // kaniko args
-    '--context',
-    `dir://${resolve(inputs.context)}`,
-    '--digest-file',
-    `${outputsDir}/digest`,
-  ]
+export function generateArgs(inputs: Inputs): string[] {
+  const args = ['--context', `dir:///context`]
 
   if (inputs.file) {
     // The docker build command resolves the Dockerfile from the context root:
@@ -98,4 +182,8 @@ function generateArgs(inputs: Inputs, outputsDir: string): string[] {
 
   args.push(...inputs.kanikoArgs)
   return args
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
